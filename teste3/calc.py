3b5ade8ed686d4945ff5dffb1f61a5b4f371958c
tiagochst <tiagochst@427e6a7c-9252-ddc5-5557-f68e940a89aa>
2011-04-24 22:23:11 +0000

    comparando cora e minstrel estranho

--- teste3/calc.py
+++ teste3/calc.py
@@ -8,11 +8,11 @@ for line in file('data.txt'):
 	copy = line	
 	line = line.split()
 	if(line):
-		if(line[0]=='T' or line[0]=='Ttp' or line[0]=='T p'):
-			print line[7]
-			print line[0]
-			if(oldCount!=line[10] or oldRate!=line[1]):
-				oldCount= line[10]
+		if(line[0]=='T' ):
+			print line[9]
+			print line[1]
+			if(oldCount!=line[9] or oldRate!=line[1]):
+				oldCount= line[9]
 				oldRate= line[1]
 	 			x0.append(copy)
 	 			rate.append(line[1])
